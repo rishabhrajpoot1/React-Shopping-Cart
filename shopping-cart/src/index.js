72908585a746a5cd5ea2b750f5a374adb1e6f997
rishabhrajpoot1 <rishabh1399904@gmail.com>
2022-06-06 13:32:20 +0530

    completed

---
+++ shopping-cart/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './stylesheets/style.css';
+import App from './Components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'))
